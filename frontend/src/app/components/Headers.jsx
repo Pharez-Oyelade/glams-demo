75638b9d08c93299309f4bd684de52a6cd7a19c6
Pharez <pharezoyelade@gmail.com>
2026-02-26 13:58:06 +0100

    Headers animation

--- frontend/src/app/components/Headers.jsx
+++ frontend/src/app/components/Headers.jsx
@@ -1,15 +1,24 @@
+"use client";
+
 import React from "react";
 import ViewBtn from "./ViewBtn";
+import { motion } from "framer-motion";
 
 const Headers = ({ title, description, text }) => {
   return (
-    <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 pb-6 sm:pb-0">
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, margin: "-100px 0px" }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 pb-6 sm:pb-0"
+    >
       <div className="pb-4 sm:pb-10">
         <h2 className="">{title}</h2>
         <p>{description}</p>
       </div>
       <ViewBtn BtnText={text} />
-    </div>
+    </motion.div>
   );
 };
 
