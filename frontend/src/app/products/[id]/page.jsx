60865df0546a6f1c8b2d71595f8d71b654d932f1
Pharez <pharezoyelade@gmail.com>
2026-03-23 22:25:42 +0100

    product detail page responsive

--- frontend/src/app/products/[id]/page.jsx
+++ frontend/src/app/products/[id]/page.jsx
@@ -24,23 +24,24 @@ const page = () => {
 
   return (
     <section>
-      <div className="grid grid-cols-2 gap-10 h-[95vh] pt-5">
+      <div className="grid grid-rows md:grid-cols-2 gap-10 md:h-[95vh] pt-5">
         {/* ................. IMAGE AND IMAGE PREVIEWS ................ */}
-        <div className="w-full h-[90%] overflow-hidden">
-          <div className="w-full h-full flex flex-row-reverse gap-3">
-            <div className="h-full w-[80%] overflow-hidden rounded-tr-[3rem] rounded-bl-[3rem]">
+        <div className="w-full md:h-[90%] md:overflow-hidden">
+          <div className="w-full h-full flex flex-col md:flex-row-reverse gap-3">
+            <div className="h-full w-full md:w-[80%] md:overflow-hidden rounded-tr-[3rem] rounded-bl-[3rem]">
               <Image
                 src={product.image}
                 width={700}
                 height={700}
                 alt={`${product.image} image`}
-                className="w-full object-cover hover:scale-105 transition-all duration-150 ease-in"
+                loading="eager"
+                className="w-full object-cover hover:scale-105 transition-all duration-150 ease-in rounded-tr-[3rem] rounded-bl-[3rem]"
               />
             </div>
 
-            <div className="h-full w-[20%] flex flex-col gap-5">
+            <div className="h-full md:w-[20%] flex md:flex-col gap-3 md:gap-5">
               {[1, 2, 3, 4, 5].map((p) => (
-                <div key={p} className="overflow-hidden">
+                <div key={p} className="md:overflow-hidden">
                   <Image
                     src={product.image}
                     width={700}
@@ -100,7 +101,7 @@ const page = () => {
             </div>
 
             {/* ................ Quantity ................. */}
-            <div className="w-[20%] space-y-2">
+            <div className="w-[35%] md:w-[20%] space-y-2">
               <h3 className="font-bold">Quantity</h3>
               <div>
                 <div className="flex justify-between gap-5 border py-2 px-3">
@@ -172,7 +173,7 @@ const page = () => {
             </p>
           </div>
 
-          <div className="flex gap-2 text-gray-600">
+          <div className="flex gap-2 text-gray-600 pb-5">
             <ShieldCheck />
             <span>Secure payment with Paystack</span>
           </div>
